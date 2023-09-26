fbba2f7bcedbd57e3f1330f1d691399c969a7d24
tocsi1000 <jakucsr@gmail.com>
2023-09-26 21:01:15 +0200

    Test commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { LinksComponent } from './links/links.component';
+import { SchoolsComponent } from './schools/schools.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo:'home', pathMatch:'full'},
+  {path: 'home', component:HomeComponent},
+  {path: 'experience', component:ExperienceComponent},
+  {path: 'projects', component:LinksComponent},
+  {path: 'schools', component:SchoolsComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SchoolsComponent } from './schools/schools.component';
+import { LinksComponent } from './links/links.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ExperienceComponent,
+    SchoolsComponent,
+    LinksComponent
   ],
   imports: [
     BrowserModule,
